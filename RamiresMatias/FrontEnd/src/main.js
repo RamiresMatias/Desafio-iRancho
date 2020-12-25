bfc891f6e4a3bf533158f40fc20629f762d1c9b9
Ramires Matias <ramiresmatias20000@gmail.com>
2020-12-25 17:39:41 -0300

    commit iRancho

---
+++ RamiresMatias/FrontEnd/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router/router.js'
+import store from './store'
+import './plugins/axios.js'
+
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
